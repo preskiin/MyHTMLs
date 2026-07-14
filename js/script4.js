024cd2c98648f29e545b01bc1222a9d39e93ac52
preskiin <redkin-sm@kompas.aero>
2026-07-14 15:58:04 +0500

    Rubilded script-file for snake.

--- js/script4.js
+++ js/script4.js
@@ -1,128 +1,142 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-canvas.width = document.body.clientWidth-5;
-canvas.height = document.body.clientHeight-5;
-DrawField();
-function resize() {
+function Main(){
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
   canvas.width = document.body.clientWidth-5;
   canvas.height = document.body.clientHeight-5;
+  window.addEventListener('resize', resize);
   DrawField();
-}
-window.addEventListener('resize', resize);
-function DrawField(){
-  ctx.fillStyle = 'rgba(0,0,0,1)'
-  ctx.fillRect(0,0, canvas.width, canvas.height);
-}
+  let flagUp = false;
+  let flagLeft = false;
+  let flagDown = false;
+  let flagRight = false;
+  const upElement = document.querySelector('.key-up');
+  const leftElement = document.querySelector('.key-left');
+  const downElement = document.querySelector('.key-down');
+  const rightElement = document.querySelector('.key-right');
+  document.addEventListener('keydown', KeyPressed);
 
-let flagUp = false;
-let flagLeft = false;
-let flagDown = false;
-let flagRight = false;
-const upElement = document.querySelector('.key-up');
-const leftElement = document.querySelector('.key-left');
-const downElement = document.querySelector('.key-down');
-const rightElement = document.querySelector('.key-right');
-document.addEventListener('keydown', KeyPressed);
-function KeyPressed(e){
-  switch (e.key){
-    case 'w':
-    case 'ц':
-    case 'ArrowUp':
-      PressUp();
-      break;
-    case 'a':
-    case 'ф':
-    case 'ArrowLeft':
-      PressLeft();
-      break;
-    case 's':
-    case 'ы':
-    case 'ArrowDown':
-      PressDown();
-      break;
-    case 'd':
-    case 'в':
-    case 'ArrowRight':
-      PressRight();
-      break;
-    default:
-      console.log('Unknown key');
-      break;
+  //Changing the size of canvas
+  function resize() {
+    canvas.width = document.body.clientWidth-5;
+    canvas.height = document.body.clientHeight-5;
+    DrawField();
   }
-}
-function PressUp(){
-  if (flagUp===false) {
+  //Filling canvas black background
+  function DrawField(){
+    ctx.fillStyle = 'rgba(0,0,0,1)'
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+  }
+  //The function to track the status of button pulled down
+  function KeyPressed(e){
+    switch (e.key){
+      case 'w':
+      case 'ц':
+      case 'ArrowUp':
+        PressUp();
+        break;
+      case 'a':
+      case 'ф':
+      case 'ArrowLeft':
+        PressLeft();
+        break;
+      case 's':
+      case 'ы':
+      case 'ArrowDown':
+        PressDown();
+        break;
+      case 'd':
+      case 'в':
+      case 'ArrowRight':
+        PressRight();
+        break;
+      default:
+        console.log('Unknown key');
+        break;
+    }
+  }
+  //Pull down up btn
+  function PressUp(){
+    if (flagUp===false) {
+      upElement.classList.toggle('pressed');
+      flagUp=true;
+      document.addEventListener('keyup', KeyRelease)
+    }
+  }
+  //Pull down left btn
+  function PressLeft(){
+    if (flagLeft===false) {
+      leftElement.classList.toggle('pressed');
+      flagLeft=true;
+      document.addEventListener('keyup', KeyRelease)
+    }
+  }
+  //Pull down down btn
+  function PressDown(){
+    if (flagDown===false) {
+      downElement.classList.toggle('pressed');
+      flagDown=true;
+      document.addEventListener('keyup', KeyRelease)
+    }
+  }
+  //Pull down right btn
+  function PressRight(){
+    if (flagRight===false) {
+      rightElement.classList.toggle('pressed');
+      flagRight=true;
+      document.addEventListener('keyup', KeyRelease)
+    }
+  }
+  //The function to track the status of released btn
+  function KeyRelease(e) {
+    switch (e.key){
+      case 'w':
+      case 'ц':
+      case 'ArrowUp':
+        ReleaseUp();
+        break;
+      case 'a':
+      case 'ф':
+      case 'ArrowLeft':
+        ReleaseLeft();
+        break;
+      case 's':
+      case 'ы':
+      case 'ArrowDown':
+        ReleaseDown();
+        break;
+      case 'd':
+      case 'в':
+      case 'ArrowRight':
+        ReleaseRight();
+        break;
+      default:
+        console.log('Unknown key');
+        break;
+    }
+  }
+  //Release for button up
+  function ReleaseUp(){
     upElement.classList.toggle('pressed');
-    flagUp=true;
-    document.addEventListener('keyup', KeyRelease)
+    flagUp=false;
+    document.removeEventListener('keyup', ReleaseUp);
   }
-}
-function PressLeft(){
-  if (flagLeft===false) {
+  //Release for button left
+  function ReleaseLeft(){
     leftElement.classList.toggle('pressed');
-    flagLeft=true;
-    document.addEventListener('keyup', KeyRelease)
+    flagLeft=false;
+    document.removeEventListener('keyup', ReleaseLeft);
   }
-}
-function PressDown(){
-  if (flagDown===false) {
+  //Release for button down
+  function ReleaseDown(){
     downElement.classList.toggle('pressed');
-    flagDown=true;
-    document.addEventListener('keyup', KeyRelease)
+    flagDown=false;
+    document.removeEventListener('keyup', ReleaseDown);
   }
-}
-function PressRight(){
-  if (flagRight===false) {
+  //Release for button right
+  function ReleaseRight(){
     rightElement.classList.toggle('pressed');
-    flagRight=true;
-    document.addEventListener('keyup', KeyRelease)
+    flagRight=false;
+    document.removeEventListener('keyup', ReleaseRight);
   }
 }
-function KeyRelease(e) {
-  switch (e.key){
-    case 'w':
-    case 'ц':
-    case 'ArrowUp':
-      ReleaseUp();
-      break;
-    case 'a':
-    case 'ф':
-    case 'ArrowLeft':
-      ReleaseLeft();
-      break;
-    case 's':
-    case 'ы':
-    case 'ArrowDown':
-      ReleaseDown();
-      break;
-    case 'd':
-    case 'в':
-    case 'ArrowRight':
-      ReleaseRight();
-      break;
-    default:
-      console.log('Unknown key');
-      break;
-  }
-}
-function ReleaseUp(){
-  upElement.classList.toggle('pressed');
-  flagUp=false;
-  document.removeEventListener('keyup', ReleaseUp);
-}
-function ReleaseLeft(){
-  leftElement.classList.toggle('pressed');
-  flagLeft=false;
-  document.removeEventListener('keyup', ReleaseLeft);
-}
-function ReleaseDown(){
-  downElement.classList.toggle('pressed');
-  flagDown=false;
-  document.removeEventListener('keyup', ReleaseDown);
-}
-function ReleaseRight(){
-  rightElement.classList.toggle('pressed');
-  flagRight=false;
-  document.removeEventListener('keyup', ReleaseRight);
-}
-DrawField();
+Main();
